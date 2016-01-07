852d7f1d53682ea97f76aa23dca96f55c1aa5b25
Florian Wessels <f.wessels@bitmotion.de>
2016-01-07 15:00:59 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_fa".
+ *
+ * Auto generated 23-04-2015 13:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Static Info Tables (fa)',
+	'description' => '(fa) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'author' => 'Rene Fritz',
+	'author_email' => 'r.fritz@colorcube.de',
+	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'version' => '0.0.0',
+	'_md5_values_when_last_written' => 'a:7:{s:20:"class.ext_update.php";s:4:"d161";s:21:"ext_conf_template.txt";s:4:"2ebd";s:12:"ext_icon.gif";s:4:"639f";s:14:"ext_tables.php";s:4:"feb2";s:14:"ext_tables.sql";s:4:"5964";s:28:"ext_tables_static_update.sql";s:4:"d5cf";s:16:"locallang_db.php";s:4:"0d9d";}',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'static_info_tables' => '',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
---
+++ ext_tables.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE static_countries (
+  cn_short_fa varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_currencies (
+  cu_name_fa varchar(50) DEFAULT '' NOT NULL,
+  cu_sub_name_fa varchar(20) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_languages (
+  lg_name_fa varchar(30) DEFAULT '0' NOT NULL
+);
+
+CREATE TABLE static_territories (
+  tr_name_fa varchar(30) DEFAULT '0' NOT NULL
+);
