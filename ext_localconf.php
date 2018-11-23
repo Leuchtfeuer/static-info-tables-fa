8f18f25130019096c6a6a1ca2a3342778be0f3cd
Florian Wessels <f.wessels@bitmotion.de>
2018-11-23 10:26:07 +0100

    [BUGFIX] Use proper path to typoscript file

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die;
 
 $initialize = function ($extKey) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
 };
 $initialize(\Bitmotion\StaticInfoTablesFa\Extension::EXTENSION_KEY);
