b7ce78d34fb42d6f39bda11a59c0365c25d30c82
Florian Wessels <f.wessels@bitmotion.de>
2018-11-23 09:53:30 +0100

    [TASK] Release of version 6.7.0

--- Classes/Domain/Model/Currency.php
+++ Classes/Domain/Model/Currency.php
@@ -1,93 +1,31 @@
 <?php
+declare(strict_types=1);
+
 namespace SJBR\StaticInfoTables\Domain\Model;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2011-2012 Armin Rüdiger Vieweg <info@professorweb.de>
- *  (c) 2013 Stanislas Rolland <typo3(arobas)sjbr.ca>
- *  (c) 2017 Florian Wessels <typo3-ext@bitmotion.de>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
-/**
- * The Currency model
- *
- * @copyright Copyright belongs to the respective authors
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- */
 class Currency extends AbstractEntity
 {
-	/**
-	 * Farsi name of the currency
-	 * @var string
-	 */
-	protected $nameFa = '';
+    protected $nameFa = '';
 
-	/**
-	 * Farsi name of the currency subdivision unit
-	 * @var string
-	 */
-	protected $subdivisionNameFa = '';
+    protected $subdivisionNameFa = '';
 
-	/**
-	 * Sets the Farsi name of the currency
-	 *
-	 * @param string $nameFa
-	 *
-	 * @return void
-	 */
-	public function setNameFa($nameFa)
+    public function getNameFa(): string
     {
-		$this->nameFa = $nameFa;
-	}
+        return $this->nameFa;
+    }
 
-	/**
-	 * Gets the Farsi name of the currency
-	 *
-	 * @return string
-	 */
-	public function getNameFa()
+    public function setNameFa(string $nameFa)
     {
-		return $this->nameFa;
-	}
+        $this->nameFa = $nameFa;
+    }
 
-	/**
-	 * Sets the Farsi name of the currency subdivision
-	 *
-	 * @param string $subdivisionNameFa
-	 *
-	 * @return void
-	 */
-	public function setSubdivisionNameFa($subdivisionNameFa)
+    public function getSubdivisionNameFa(): string
     {
-		$this->subdivisionNameFa = $subdivisionNameFa;
-	}
+        return $this->subdivisionNameFa;
+    }
 
-	/**
-	 * Gets the Farsi name of the currency subdivision
-	 *
-	 * @return string
-	 */
-	public function getSubdivisionNameFa()
+    public function setSubdivisionNameFa(string $subdivisionNameFa)
     {
-		return $this->subdivisionNameFa;
-	}
+        $this->subdivisionNameFa = $subdivisionNameFa;
+    }
 }
---
+++ Classes/Extension.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Bitmotion\StaticInfoTablesFa;
+
+class Extension
+{
+    const EXTENSION_KEY = 'static_info_tables_fa';
+}
