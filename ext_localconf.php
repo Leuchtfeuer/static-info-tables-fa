796ebb713c3da8b5440f2272951509def2947eb3
Florian Wessels <f.wessels@bitmotion.de>
2018-11-23 09:58:10 +0100

    [BUGFIX] Use proper namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,5 +5,5 @@ $initialize = function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
 };
-$initialize(\Bitmotion\StaticInfoTablesCs\Extension::EXTENSION_KEY);
+$initialize(\Bitmotion\StaticInfoTablesFa\Extension::EXTENSION_KEY);
 unset($initialize);
