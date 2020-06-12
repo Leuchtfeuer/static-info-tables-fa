f80f395c6a2cd9a2aaed8c4dbd047f6140f5b6cf
Florian Wessels <flo.wessels@gmail.com>
2020-06-12 11:45:03 +0200

    [TASK] Apply CS

--- Classes/Extension.php
+++ Classes/Extension.php
@@ -1,18 +1,17 @@
 <?php
-declare(strict_types = 1);
 
-namespace Bitmotion\StaticInfoTablesFa;
+declare(strict_types=1);
 
-/***
- *
- * This file is part of the "Static Info Tables (FA)" Extension for TYPO3 CMS.
+/*
+ * This file is part of the "Static Info Tables (FA)" extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- *  (c) 2018 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
- *
- ***/
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+namespace Bitmotion\StaticInfoTablesFa;
 
 class Extension
 {
